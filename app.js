ff45d1486bf392e7c03eeaa78ea8fc34f55fb7ea
MuhammadAhmadAslam <prince1ahmed111@gmail.com>
2024-03-22 22:59:58 +0500

    Star Pattern

---
+++ app.js
@@ -0,0 +1,173 @@
+for(var i = 1; i<= 7; i++){
+    for(var j = i; j<= 7; j++){
+      document.write('●' + ' ');
+    }
+    document.write('<br>');
+  }
+  
+  document.write('<br>');
+  
+  for(var i = 1; i<= 7; i++){
+    for(var j = 1; j<= i; j++){
+      document.write('●' + ' ');
+    }
+    document.write('<br>')
+  }
+  
+  
+  document.write('<center>');
+  
+  for(let i = 1; i <= 8; i++){
+    for(let j = 1; j<= i; j++){
+      document.write('●' + ' ');
+    }
+    document.write('<br>')
+  }
+  document.write('</center>');
+  document.write('<br>')
+  document.write('<br>')
+  document.write('<br>')
+  document.write('<div class="hello">')
+  
+  for(let i = 1; i<= 7; i++){
+    for(let j = 1; j<= i; j++){
+      document.write('●' + ' ');
+    }
+    document.write('<br>')
+  }
+  
+  
+  document.write('</div>');
+  document.write('<br>')
+  document.write('<br>')
+  document.write('<br>')
+  
+  document.write("<div class='pattern'>");
+  for(let i = 7; i>= 1; i--){
+    for(let j = 1; j<= i; j++){
+      document.write('●' + ' ');
+    }
+    document.write('<br>')
+  }
+  document.write("<div class='pattern1'>");
+  for(let i = 7; i>= 1; i--){
+    for(let j= 1; j<= i; j++){
+      document.write('●' + ' ');
+    }
+    document.write('<br>')
+  }
+  document.write("</div>")
+  document.write("</div>");
+  
+  
+  
+  
+  document.write("<div class='pattern2'>");
+  for(let i = 1; i<= 7; i++){
+    for(let j = 1; j<= i; j++){
+      document.write("●" + ' ');
+    }
+    document.write('<br>')
+  }
+  document.write("<div class='pattern3'>");
+  
+  for(let i= 1; i<= 7; i++){
+    for(let j=1; j<= i; j++){
+      document.write('●' + ' ');
+    }
+    document.write('<br>');
+  }
+  
+  document.write("</div>");
+  
+  document.write("</div>");
+  
+  
+  document.write('<br>')
+  document.write('<br>')
+  document.write('<br>')
+  document.write('<center>');
+  
+  for(let i = 7; i>=1; i--){
+    for(let j = 1; j<= i; j++){
+      document.write('●' + " ");
+    }
+    document.write('<br>')
+  }
+  
+  
+  
+  for(let i = 1; i<= 7; i++){
+    for(let j = 1; j<= i; j++){
+      document.write('●' + ' ');
+    }
+    document.write('<br>')
+  }
+  
+  
+  
+  document.write('<br>')
+  document.write('<br>')
+  document.write('<br>')
+  document.write('<br>')
+  
+  
+  
+  for(let i =1; i<= 7; i++){
+    for(let j = 1; j<= i; j++){
+      document.write('●' + ' ');
+    }
+    document.write('<br>')
+  }
+  for(let i=1; i<= 7; i++){
+    for(let j=7; j>=i; j--){
+      document.write('●' + ' ');
+    }
+    document.write('<br>')
+  }
+  
+  
+  
+  document.write('</center>');
+  
+  
+  document.write('<br>')
+  document.write('<br>')
+  document.write('<br>')
+  
+ for (let i = 1; i <= 7; i++) {
+    for (let j = 1; j <= i; j++) {
+        document.write("*" + " ");
+        
+    }
+    document.write("<br>")
+ }
+ for (let i = 1; i <= 7; i++) {
+    for (let j = 7; j >= i; j--) {
+        document.write("*" + " ")
+        
+    }
+    document.write("<br>")
+ }
+
+
+document.write("<div class='last'>")
+ document.write('<br>')
+  document.write('<br>')
+  document.write('<br>')
+
+for (let i = 1; i <= 7; i++) {
+    for (let j = 1; j<= i; j++) {
+        document.write("*" + " ");
+        
+    }
+    document.write("<br>")
+}  
+for (let i = 1; i <= 7; i++) {
+    for (let j = 7; j >= i; j--) {
+        document.write("*" + " ")
+        
+    }
+    document.write("<br>")
+}
+document.write("</div>")
